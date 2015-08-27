6ddb1ebb93c667a64adf0844c91591e783856f80
Tim Whitacre <tim@timw.co>
2015-08-26 23:19:17 -0400

    Adding in login process

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCookies'])
 
 .constant('PARSE', {
   URL: 'https://api.parse.com/1/',
@@ -10,7 +10,7 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $cookies, UserService, $rootScope) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -40,84 +40,104 @@ angular.module('starter.controllers', [])
     $scope.modal.show();
   };
 
+  $scope.logout = function () {
+    UserService.logout();
+  };
+
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
+    UserService.login($scope.loginData);
+
+    $rootScope.$on('user:loggedin', function () {
       $scope.closeLogin();
-    }, 1000);
+    });
   };
+
+  $rootScope.$on('user:statusChange', function () {
+    if(UserService.isLoggedIn()) {
+      $scope.loggedIn = true;
+    } else {
+      $scope.loggedIn = false;
+    }
+  });
+
+
 })
 
-.controller('DayController', function ($scope, $stateParams, PARSE, $http, $state) {
+.controller('DayController', function ($scope, $stateParams, PARSE, $http, $state, UserService) {
 
   $scope.day = $stateParams.day;
   $scope.excercises;
 
-  $http({
-    method: 'GET',
-    url: PARSE.URL + 'classes/excercise',
-    params: {
-      where: '{"day":"' + $stateParams.day + '"}'
-    },
-    headers: PARSE.CONFIG.headers
-  }).success( function (res) {
-    $scope.excercises = res.results;
-  });
+  if (UserService.isLogedIn()) {
+    $http({
+      method: 'GET',
+      url: PARSE.URL + 'classes/excercise',
+      params: {
+        where: '{"day":"' + $stateParams.day + '"}'
+      },
+      headers: PARSE.CONFIG.headers
+    }).success( function (res) {
+      $scope.excercises = res.results;
+    });
 
-  $scope.loadExcercise = function (single) {
-    $state.go('app.excercise', { id: single.objectId });
-  };
+    $scope.loadExcercise = function (single) {
+      $state.go('app.excercise', { id: single.objectId });
+    };
+  }
+
 
 })
 
-.controller('ExcerciseController', function ($scope, $stateParams, PARSE, $http) {
+.controller('ExcerciseController', function ($scope, $stateParams, PARSE, $http, UserService) {
 
   $scope.excercise;
   $scope.message;
 
-  $http({
-    method: 'GET',
-    url: PARSE.URL + 'classes/excercise/' + $stateParams.id,
-    headers: PARSE.CONFIG.headers,
-    cache: true
-  })
-  .success( function (res) {
-    $scope.excercise = res;
-  });
+  if (UserService.isLogedIn()) {
 
-  // Add Entry
-  $scope.entry = {
-    set: 1, reps: 10
-  };
-  $scope.addEntry = function (entry, excercise) {
-
-    entry.set = Number(entry.set);
-    entry.reps = Number(entry.reps);
-
-    var data = {
-      excercise: {
-        '__type': 'Pointer',
-        'className': 'excercise',
-        'objectId': excercise.objectId,
-      },
-      set: entry.set,
-      reps: entry.reps,
-      weight: entry.weight
-    }
-
-    $http.post(PARSE.URL + 'classes/entry', data, PARSE.CONFIG)
+    $http({
+      method: 'GET',
+      url: PARSE.URL + 'classes/excercise/' + $stateParams.id,
+      headers: PARSE.CONFIG.headers,
+      cache: true
+    })
     .success( function (res) {
-      // Clear Data & Show Message
-      $scope.entry = { set: 1, reps: 10 };
-      $scope.message = 'Logged Set: ' + entry.set;
+      $scope.excercise = res;
     });
 
+    // Add Entry
+    $scope.entry = {
+      set: 1, reps: 10
+    };
+    $scope.addEntry = function (entry, excercise) {
 
-  };
+      entry.set = Number(entry.set);
+      entry.reps = Number(entry.reps);
+
+      var data = {
+        excercise: {
+          '__type': 'Pointer',
+          'className': 'excercise',
+          'objectId': excercise.objectId,
+        },
+        set: entry.set,
+        reps: entry.reps,
+        weight: entry.weight
+      }
+
+      $http.post(PARSE.URL + 'classes/entry', data, PARSE.CONFIG)
+      .success( function (res) {
+        // Clear Data & Show Message
+        $scope.entry = { set: 1, reps: 10 };
+        $scope.message = 'Logged Set: ' + entry.set;
+      });
+
+    };
+
+  }
 
 })
 
---
+++ www/js/services.js
@@ -0,0 +1,48 @@
+angular.module('starter.controllers')
+
+.service('UserService', function ($cookies, PARSE, $http, $state, $rootScope) {
+
+  this.checkStatus = function () {
+    var token = $cookies.get('session-token');
+    if(token) {
+      PARSE.CONFIG.headers['X-Parse-Session-Token'] = token;
+      $rootScope.$broadcast('user:statusChange');
+    } else {
+      $rootScope.$broadcast('user:statusChange');
+      $state.go('app.home');
+    }
+  };
+
+  this.isLoggedIn = function () {
+    if ($cookies.get('session-token')) {
+      return true;
+    }
+    return false;
+  }
+
+  this.login = function (user) {
+
+    var self = this;
+
+    $http({
+      method: 'GET',
+      url: PARSE.URL + 'login',
+      headers: PARSE.CONFIG.headers,
+      params: user
+    }).success( function (data) {
+      console.log(data);
+      $cookies.put('session-token', data.sessionToken);
+      self.checkStatus();
+      $rootScope.$broadcast('user:loggedin');
+    });
+
+  };
+
+  this.logout = function () {
+    PARSE.CONFIG.headers['X-Parse-Session-Token'] = null;
+    $cookies.remove('session-token');
+    $rootScope.$broadcast('user:statusChange');
+  };
+
+
+})
